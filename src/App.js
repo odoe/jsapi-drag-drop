c10319af6ce7b838f8467090f847e82c4d7f7ad3
Rene Rubalcava <rrubalcava@esri.com>
2021-10-28 12:11:28 -0700

    add editing

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import "./App.css";
 function App() {
 
   const onDragStarted = (e) => {
-    e.dataTransfer.setData("image", e.target.src);
+    e.dataTransfer.setData("data", e.target.dataset.severity);
   }
 
   return (
     <div className="mapContainer">
       <MapDiv />
       <div className="listArea" onDragStart={onDragStarted}>
-        <img id="symbol" src="https://arcgis.github.io/arcgis-samples-javascript/sample-data/layers-ogcfeaturelayer/windmill.png" height="50px" width="50px" alt="Symbol" />
+        <img data-severity="1" id="symbol" src="https://arcgis.github.io/arcgis-samples-javascript/sample-data/cat1.png" height="50px" width="50px" alt="Symbol" />
+        <img data-severity="2" id="symbol" src="https://arcgis.github.io/arcgis-samples-javascript/sample-data/cat2.png" height="50px" width="50px" alt="Symbol" />
+        <img data-severity="3" id="symbol" src="https://arcgis.github.io/arcgis-samples-javascript/sample-data/cat3.png" height="50px" width="50px" alt="Symbol" />
       </div>
     </div>
   );
--- src/MapDiv.js
+++ src/MapDiv.js
@@ -1,6 +1,9 @@
 import { useRef, useEffect } from "react";
 import MapView from "@arcgis/core/views/MapView";
-import WebMap from "@arcgis/core/WebMap";
+import ArcGISMap from "@arcgis/core/Map";
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
+import Legend from "@arcgis/core/widgets/Legend";
+import { distance, equals } from "@arcgis/core/geometry/geometryEngine";
 
 import "./MapDiv.css";
 
@@ -9,42 +12,67 @@ function MapDiv() {
 
   useEffect(() => {
     if (mapDiv.current) {
-      const webmap = new WebMap({
+      const layer = new FeatureLayer({
         portalItem: {
-          id: "aa1d3f80270146208328cf66d022e09c",
+          id: "c84e906801534a6eaed537b7a9ae8ef1",
         },
+        outFields: ["severity"],
+      });
+
+      const webmap = new ArcGISMap({
+        basemap: "dark-gray-vector",
+        layers: [layer],
       });
 
       const view = new MapView({
         container: mapDiv.current,
         map: webmap,
+        center: [-86.008, 39.96585],
+        zoom: 14,
       });
 
-      view.when(() => {
+      const legend = new Legend({ view });
+
+      view.ui.add(legend, "bottom-left");
+
+      view.when(async () => {
         console.log("view ready");
-      });
+        const layerView = await view.whenLayerView(layer);
 
-      view.container.addEventListener("dragover", (event) => {
-        event.preventDefault();
-        event.dataTransfer.dropEffect = "copy";
-      });
-      view.container.addEventListener("drop", (event) => {
-        event.preventDefault();
-        event.stopPropagation();
-        console.log(event);
-        const imageURL = event.dataTransfer.getData("image");
-        const geometry = view.toMap(event);
-        const graphic = {
-            geometry,
-            symbol: {
-                type: "picture-marker",
-                url: imageURL,
-                height: 50,
-                width: 50
-            }
-        };
-        console.log(graphic)
-        view.graphics.add(graphic);
+        view.container.addEventListener("dragover", (event) => {
+          event.preventDefault();
+          event.dataTransfer.dropEffect = "copy";
+        });
+        view.container.addEventListener("drop", async (event) => {
+          event.preventDefault();
+          // data value to update attribute
+          const severity = event.dataTransfer.getData("data");
+          const geometry = view.toMap(event);
+
+          const query = layerView.layer.createQuery();
+          query.outFields = ["*"];
+          query.geometry = geometry;
+          query.distance = 200;
+          query.units = "meters";
+
+          // find nearby features
+          const { features } = await layerView.queryFeatures(query);
+          // iterate over point geometries
+          // find the closest feature to where
+          // drop on map occured
+          const coords = features.map((x) => x.geometry);
+          const distances = coords.map((coord) => distance(coord, geometry));
+          const min = Math.min(...distances);
+          const index = distances.findIndex((x) => x === min);
+          const coord = coords[index];
+          const feature = features.find((feat) => equals(feat.geometry, coord));
+
+          feature.attributes.severity = severity;
+
+          layer.applyEdits({
+            updateFeatures: [feature],
+          });
+        });
       });
     }
   }, [mapDiv]);
